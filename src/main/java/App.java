438e4d54d8692c5920f20a0331dbf2deaca8d8a3
justforit-help <soundous nidar>
2026-01-09 00:04:11 +0100

    Modification sur dev - nouveau message

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Bonjour Monsieur c est SOUNDOUS :)");
+        System.out.println("Bonjour Monsieur c est SOUNDOUS - Branche DEV :)");
     }
 }
